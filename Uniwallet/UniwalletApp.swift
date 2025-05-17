22c6dcba4f32e6af2099d0895866a1f0be3bba5f
AdrianNeshad <Adrian.neshad1@gmail.com>
2025-05-17 11:16:56 +0200

    Initial Commit

---
+++ Uniwallet/UniwalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniwalletApp.swift
+//  Uniwallet
+//
+//  Created by Adrian Neshad on 2025-05-17.
+//
+
+import SwiftUI
+
+@main
+struct UniwalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
